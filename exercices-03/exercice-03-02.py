eb6432db905d626221f699c5b8f29b8fcb276d34
Alexandre <Alechemin@gmail.com>
2023-06-06 13:33:29 +0200

    Mise à jour

--- exercices-03/exercice-03-02.py
+++ exercices-03/exercice-03-02.py
@@ -12,7 +12,7 @@ chocolates = 17
 friends = 3
 
 # réponse 3.2
-candies_rest = can
-chocolates_rest = 
+candies_rest = candies % friends
+chocolates_rest = chocolates % friends
 print(candies_rest)
 print(chocolates_rest)
